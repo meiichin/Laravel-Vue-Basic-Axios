c9da98937e6fadb260cc9370755aaf272d9e3b4c
meiichin <meiichan3017@gmail.com>
2019-03-16 22:31:35 +0700

    Add to repo

---
+++ resources/js/components/Users.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Users Component</div>
+
+                    <div class="card-body">
+                        <p v-for="user in users">{{user.name}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                users:[]
+            }
+        },
+        mounted(){
+
+        }
+    }
+</script>
